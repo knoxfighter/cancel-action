6a14fced512119b7748f41a45c9da63698afaf0c
knoxfighter <knoxfighter@asdff45.de>
2021-01-29 05:10:19 +0100

    Show the created link

--- cancel.js
+++ cancel.js
@@ -1,8 +1,10 @@
 const https = require('https');
+const link = `/repos/${process.env.GITHUB_REPOSITORY_OWNER}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}/cancel`;
+console.log(link);
 const options = {
   hostname: 'api.github.com',
   // path: `/repos/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}/cancel`,
-  path: `/repos/${process.env.GITHUB_REPOSITORY_OWNER}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}/cancel`,
+  path: link,
   headers: {
     'Authorization': `token ${process.env.INPUT_TOKEN}`,
     'Content-Type': 'application/json',
